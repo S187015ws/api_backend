9b4b86e3548781d178d8b69e09abb9acc744d173
S187015ws <187015689@stu.vtc.edu.hk>
2019-04-25 23:02:53 -0700

    init controllers

---
+++ controllers/sell.js
@@ -0,0 +1,14 @@
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = "mongodb+srv://304CEMe:123abc@cluster0-rmfhf.azure.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "CDDB"; 
+var database, collection
+
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+        throw error;         
+    }         
+    database = client.db(DATABASE_NAME);
+    collection = database.collection("SELL");
+    console.log("Connected to `" + DATABASE_NAME + "`!");
+}); 
